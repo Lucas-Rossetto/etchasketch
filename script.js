f9926c5d62fd8f013c0dfc3331e8850e806aa211
lucasr <rossetto.luc@gmail.com>
2024-08-07 21:14:35 +0200

    grid with button to refresh the grid

--- script.js
+++ script.js
@@ -15,9 +15,31 @@ window.onload = function() {
     }
 }
 
+function popUp() {
+    
+    const newGrid = document.createElement("div");
+    document.getElementById('container').appendChild(newGrid)
+    let promptValue = prompt();
+    let promptValueNumber = Number(promptValue);
+    grid.remove()
+    console.log(grid)
+    console.log(newGrid)
+    if(promptValueNumber <= 100) {
+        for (let i = 0; i < promptValueNumber; i++) {
+            let row = document.createElement("div");
+            row.className= "row";
+            for (let j = 0; j < promptValueNumber; j++) {
+                let cell = document.createElement("div");
+                cell.className = "gridSquare";
+                row.appendChild(cell)
 
-function divGenerators(numberOfDiv) {
-    for (let i = 0; i < numberOfDiv; i++) {
+            }
+            newGrid.appendChild(row)
+        }
+    }
+    grid = newGrid
+
+   /* for (let i = 0; i < numberOfDiv; i++) {
         let row = document.createElement("div");
         row.className= "row";
         for (let j = 0; j < numberOfDiv; j++) {
@@ -27,6 +49,6 @@ function divGenerators(numberOfDiv) {
         }
         grid.appendChild(row)
         console.log(row)
-    }
+    }*/
    
 }
