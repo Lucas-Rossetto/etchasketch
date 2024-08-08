38e7eed475ed9731328095caee2870c837a5ac4c
lucasr <rossetto.luc@gmail.com>
2024-08-08 19:22:19 +0200

    grid finished but a bit laggy for some reason

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
-let grid = document.createElement("div");
-document.getElementById('container').appendChild(grid)
+let container = document.getElementById('container')
+let comp = container.currentStyle || getComputedStyle(container,null)
 
 window.onload = function() {
     let numberOfDiv = 16
     for (let i = 0; i < numberOfDiv; i++) {
-        let row = document.createElement("div");
-        row.className= "row";
         for (let j = 0; j < numberOfDiv; j++) {
-            let cell = document.createElement("div");
-            cell.className = "gridSquare";
-            row.appendChild(cell)
+             let cell = document.createElement("div");
+             cell.className = "gridSquare";
+             let cellStyle = getComputedStyle(cell,null)
+             let cellStyleWidthValue = cellStyle.width.replace(/\D/g, '');
+             cellStyleWidthValue = comp.width.replace(/\D/g, '') / numberOfDiv
+             let cellStyleWidthString = cellStyleWidthValue.toString() + "px"
+             cell.style.width = cellStyleWidthString
+             container.appendChild(cell)
+
         }
-        grid.appendChild(row)
     }
 }
 
 function popUp() {
+
     
-    const newGrid = document.createElement("div");
-    document.getElementById('container').appendChild(newGrid)
     let promptValue = prompt();
     let promptValueNumber = Number(promptValue);
-    grid.remove()
-    console.log(grid)
-    console.log(newGrid)
     if(promptValueNumber <= 100) {
+        while (container.firstChild){
+            container.removeChild(container.firstChild)
+        }
         for (let i = 0; i < promptValueNumber; i++) {
-            let row = document.createElement("div");
-            row.className= "row";
-            for (let j = 0; j < promptValueNumber; j++) {
-                let cell = document.createElement("div");
-                cell.className = "gridSquare";
-                row.appendChild(cell)
-
+            for (let j = 0; j < promptValueNumber; j++) {   
+             let cell = document.createElement("div");
+             cell.className = "gridSquare";
+             let cellStyle = getComputedStyle(cell,null)
+             let cellStyleWidthValue = cellStyle.width.replace(/\D/g, '');
+             cellStyleWidthValue = comp.width.replace(/\D/g, '') / promptValueNumber
+             let cellStyleWidthString = cellStyleWidthValue.toString() + "px"
+             cell.style.width = cellStyleWidthString
+             container.appendChild(cell)
             }
-            newGrid.appendChild(row)
         }
     }
-    grid = newGrid
-
-   /* for (let i = 0; i < numberOfDiv; i++) {
-        let row = document.createElement("div");
-        row.className= "row";
-        for (let j = 0; j < numberOfDiv; j++) {
-            let cell = document.createElement("div");
-            cell.className = "gridSquare";
-            row.appendChild(cell)
-        }
-        grid.appendChild(row)
-        console.log(row)
-    }*/
-   
 }
