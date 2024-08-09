e2d69cf62bd5b211e6a37e44c5af0dc435d65483
lucasr <rossetto.luc@gmail.com>
2024-08-09 15:31:37 +0200

    hover and js cells finished

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ window.onload = function() {
     for (let i = 0; i < numberOfDiv; i++) {
         for (let j = 0; j < numberOfDiv; j++) {
              let cell = document.createElement("div");
+             if (cell != null) {
+               let randomColor = Math.floor(Math.random()*16777215).toString(16);
+               cell.style.backgroundColor = "#FFFFFF";
+               cell.onmousemove = function () { this.style.backgroundColor = "#" + randomColor; this.style.color = "#" + randomColor; };
+               cell.onmouseout = function () { this.style.backgroundColor = "#" + randomColor; this.style.color = "#" + randomColor; };
+             }
              cell.className = "gridSquare";
              let cellStyle = getComputedStyle(cell,null)
              let cellStyleWidthValue = cellStyle.width.replace(/\D/g, '');
@@ -22,14 +28,19 @@ function popUp() {
 
     
     let promptValue = prompt();
+    if(promptValue === null) return;
     let promptValueNumber = Number(promptValue);
+    container.replaceChildren()
     if(promptValueNumber <= 100) {
-        while (container.firstChild){
-            container.removeChild(container.firstChild)
-        }
         for (let i = 0; i < promptValueNumber; i++) {
             for (let j = 0; j < promptValueNumber; j++) {   
-             let cell = document.createElement("div");
+              let cell = document.createElement("div");
+              if (cell != null) {
+                let randomColor = Math.floor(Math.random()*16777215).toString(16);
+                cell.style.backgroundColor = "#FFFFFF";
+                cell.onmousemove = function () { this.style.backgroundColor = "#" + randomColor; this.style.color = "#" + randomColor; };
+                cell.onmouseout = function () { this.style.backgroundColor = "#" + randomColor; this.style.color = "#" + randomColor; };
+              }
              cell.className = "gridSquare";
              let cellStyle = getComputedStyle(cell,null)
              let cellStyleWidthValue = cellStyle.width.replace(/\D/g, '');
@@ -41,3 +52,6 @@ function popUp() {
         }
     }
 }
+
+
+
